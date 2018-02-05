b3c3427a289c9fbdc5601399ec4f3efd8dc04427
Soadreqm <antti.virkkunen@gmail.com>
2018-02-05 02:04:04 +0200

    update usings

--- PixieGrower/Pixie.cs
+++ PixieGrower/Pixie.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PixieGrower
+﻿namespace PixieGrower
 {
     /// <summary>
     /// A pixie, bred for its magical dust
